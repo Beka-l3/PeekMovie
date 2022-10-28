bd08cd64b5c6a6401788164722fb7dcdceebdbf8
Beka_l3 <b.talgat@innopolis.university>
2022-10-28 12:48:19 +0300

    Press and hold to pause animation

--- PeekMovie/Session Module/View/Main Page/PosterView.swift
+++ PeekMovie/Session Module/View/Main Page/PosterView.swift
@@ -17,19 +17,21 @@ class PosterViewPage: UIViewController, Colors {
     weak var presenter: PosterViewDelegate?
     private var posterViewModels: PosterViewModels
     
-    
     init() {
         self.posterViewModels = PosterViewModels()
         super.init(nibName: nil, bundle: nil)
     }
     required init?(coder: NSCoder) { fatalError("init(coder:) has not been implemented") }
     
-    
+//  MARK: - lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = black
         setupViews()
         posterViewModels.infoButton.addTarget(self, action: #selector(handleInfoButton), for: .touchUpInside)
+        let longPress = UILongPressGestureRecognizer(target: self, action: #selector(handlePress))
+        longPress.minimumPressDuration = .zero
+        view.addGestureRecognizer(longPress)
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -54,14 +56,28 @@ class PosterViewPage: UIViewController, Colors {
             offset = newSize.width - posterViewSize.width
         }
         
-        
         UIView.animate(withDuration: 12, delay: 1, options: [.repeat, .autoreverse]) {
             self.posterViewModels.posterImage.frame.origin = CGPoint(x: -offset, y: 0)
         }
     }
     
+//  MARK: - objc
+    @objc func handlePress(gesture: UILongPressGestureRecognizer) {
+        if gesture.state == .began {
+            pauseLayer(layer: posterViewModels.posterImage.layer)
+        } else if gesture.state == .ended {
+            resumeLayer(layer: posterViewModels.posterImage.layer)
+        }
+    }
     
+    @objc func handleInfoButton() {
+        let opacity = posterViewModels.movieInfoView.scrollView.layer.opacity
+        UIView.animate(withDuration: 0.8, delay: 0, options: []) {
+            self.posterViewModels.movieInfoView.scrollView.layer.opacity = 1 - opacity
+        }
+    }
     
+//  MARK: - func
     private func setupViews() {
         let posterView = posterViewModels.posterView
         view.addSubview(posterView)
@@ -90,26 +106,6 @@ class PosterViewPage: UIViewController, Colors {
         posterViewModels.yellowFadeBottom.frame.size = CGSize(width: view.frame.width, height: yellowFadeHeight)
         posterViewModels.yellowFadeBottom.frame.origin = CGPoint(x: 0, y: view.frame.height - tabBarHeight)
         view.layer.addSublayer(posterViewModels.yellowFadeBottom)
-    }
-    
-    @objc func handleInfoButton() {
-        // MARK: FIX ME PLEASE
-        let opacity = posterViewModels.movieInfoView.scrollView.layer.opacity
-        
-        if opacity == 1 {
-            pauseLayer(layer: posterViewModels.posterImage.layer)
-        } else {
-            resumeLayer(layer: posterViewModels.posterImage.layer)
-        }
-        
-//        print(posterViewModels.posterImage.layer.speed)
-//        posterViewModels.posterImage.layer.speed = posterViewModels.posterImage.layer.speed == 0 ? 1 : 0
-//        print(posterViewModels.posterImage.frame.origin)
-        
-        UIView.animate(withDuration: 0.8, delay: 0, options: []) {
-            self.posterViewModels.movieInfoView.scrollView.layer.opacity = 1 - opacity
-//            self.posterViewModels.movieInfoView.whiteFadeBackground.opacity = 1 - opacity
-        }
     }
     
     func pauseLayer(layer: CALayer) {
