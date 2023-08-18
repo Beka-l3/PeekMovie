7a34c7f0f1efc61523464de003116caef81c70e9
Beka-l3 <b.talgat@innopolis.university>
2023-08-18 22:09:43 +0300

    Log

---
+++ PeekMovie/Logger/Log.swift
@@ -0,0 +1,12 @@
+//
+//  Log.swift
+//  PeekMovie
+//
+//  Created by Bekzhan Talgat on 18.08.2023.
+//
+
+import Foundation
+
+// MARK: - Logger instance
+
+let log: LoggerProtocol = makeDefaultLogger(module: "CoffeeMate")
