4568fc259c77160b43e929b3302755beb2b19d9a
Beka-l3 <b.talgat@innopolis.university>
2023-11-04 19:06:53 +0300

    Add new icons

--- PeekMovie/Utils/Colors/Colors.swift
+++ PeekMovie/Utils/Colors/Colors.swift
@@ -9,13 +9,18 @@ import UIKit
 
 
 enum Colors {
-    static let backgroundPrimary: UIColor = .init(hex: "121212FF")!
-    static let backgroundSecondary: UIColor = .init(hex: "0B0B0CFF")!
+    static let backgroundPrimary: UIColor = .init(hex: "0B0B0CFF")!
+    static let backgroundSecondary: UIColor = .init(hex: "121212FF")!
     
-    static let focus: UIColor = .init(hex: "F5FA19FF")!
+    static let focus: UIColor = .init(hex: "C6C03EFF")!
+    static let focusDark: UIColor = .init(hex: "252406FF")!
     
     static let textMain: UIColor = .init(white: 1, alpha: 0.87)
     static let textSecondary: UIColor = .init(white: 1, alpha: 0.6)
     static let textTertiary: UIColor = .init(white: 1, alpha: 0.38)
+    
+    static let textMainBlack: UIColor = .init(white: 0, alpha: 0.87)
+    static let textSecondaryBlack: UIColor = .init(white: 0, alpha: 0.6)
+    static let textTertiaryBlack: UIColor = .init(white: 0, alpha: 0.38)
     
 }
