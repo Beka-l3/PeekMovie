28830acc9b5f29d9fc73c25e640e8293cc9dea9d
Beka-l3 <b.talgat@innopolis.university>
2023-11-06 16:12:29 +0300

    Add buttons

--- PeekMovie/Modules/Entrance/SignInPage/SignInPageViewComponents.swift
+++ PeekMovie/Modules/Entrance/SignInPage/SignInPageViewComponents.swift
@@ -21,11 +21,16 @@ final class SignInPageViewComponents {
     lazy var passwordIconView = PeekIconView(image: Images.Icon.Key.x24)
     
     lazy var loginButton = PeekButton(type: .main, titleText: Constants.loginButtonTitleText)
-    lazy var SignUpButton = PeekButton(type: .secondary, titleText: Constants.signUpButtonTitleText)
-    lazy var SignInButton = PeekButton(type: .secondary, titleText: Constants.signInButtonTitleText)
+    lazy var signUpButton = PeekButton(type: .secondary, titleText: Constants.signUpButtonTitleText)
+    lazy var signInButton = PeekButton(type: .secondary, titleText: Constants.signInButtonTitleText)
     lazy var forgotPasswordButton = PeekButton(type: .tertiary, titleText: Constants.forgotPasswordButtonTitleText)
     
-//    lazy var signUpLabel = PeekLabel()
+    lazy var signUpLabel = PeekLabel(type: .secondary, text: Constants.signUpLabelText, font: .callout)
+    lazy var signInLabel = PeekLabel(type: .secondary, text: Constants.signInLabelText, font: .callout)
+    
+    lazy var signUpHStack = getButtonHStackView(with: [signUpLabel, signUpButton])
+    lazy var signInHStack = getButtonHStackView(with: [signInLabel, signInButton])
+    
     
     
     func setupViews(parent: UIView) {
@@ -38,6 +43,9 @@ final class SignInPageViewComponents {
         parent.addSubview(emailIconView)
         parent.addSubview(passwordTextField)
         parent.addSubview(passwordIconView)
+        parent.addSubview(loginButton)
+        parent.addSubview(forgotPasswordButton)
+        parent.addSubview(signInHStack)
         
         NSLayoutConstraint.activate([
             appLogoIconView.centerXAnchor.constraint(equalTo: parent.centerXAnchor),
@@ -71,6 +79,15 @@ final class SignInPageViewComponents {
             passwordIconView.widthAnchor.constraint(equalToConstant: passwordIconView.iconSize.rawValue),
             passwordIconView.heightAnchor.constraint(equalToConstant: passwordIconView.iconSize.rawValue),
             passwordIconView.trailingAnchor.constraint(equalTo: passwordTextField.leadingAnchor, constant: -Constants.paddingL),
+            
+            loginButton.leadingAnchor.constraint(equalTo: usernameTextField.leadingAnchor),
+            loginButton.topAnchor.constraint(equalTo: passwordTextField.bottomAnchor, constant: Constants.paddingXL),
+            
+            forgotPasswordButton.topAnchor.constraint(equalTo: passwordTextField.bottomAnchor, constant: Constants.paddingXS),
+            forgotPasswordButton.trailingAnchor.constraint(equalTo: parent.safeAreaLayoutGuide.trailingAnchor, constant: -Constants.paddingXS),
+            
+            signInHStack.centerXAnchor.constraint(equalTo: parent.centerXAnchor),
+            signInHStack.bottomAnchor.constraint(equalTo: parent.bottomAnchor, constant: -Constants.paddingL),
         ])
     }
     
@@ -91,6 +108,9 @@ extension SignInPageViewComponents {
         static let signInButtonTitleText = "Sign In"
         static let forgotPasswordButtonTitleText = "forgot password?"
         
+        static let signUpLabelText = "If you do not have a peek id"
+        static let signInLabelText = "If you have a peek id"
+        
         static let appLogoIconViewYOrigin: CGFloat = 96
         static let inputBlockYOrigin: CGFloat = 342
         
@@ -101,5 +121,22 @@ extension SignInPageViewComponents {
         static let paddingL: CGFloat = 36
         static let paddingXL: CGFloat = 48
         static let paddingXXL: CGFloat = 52
+    }
+}
+
+
+extension SignInPageViewComponents {
+    private func getButtonHStackView(with arrangedSubviews: [UIView]) -> UIStackView {
+        let view = UIStackView()
+        view.axis = .horizontal
+        view.spacing = Constants.paddingXXS
+        view.backgroundColor = Colors.clearBlack
+        
+        for subview in arrangedSubviews {
+            view.addArrangedSubview(subview)
+        }
+        
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
     }
 }
