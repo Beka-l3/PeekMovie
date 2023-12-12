f0dc3524b1ae11a6e81521a9f5a49c5ac39ea78d
Beka-l3 <b.talgat@innopolis.university>
2023-12-12 15:23:53 +0300

    Initial components

--- PeekMovie/Modules/Lobby/LobbyPage/ViewComponents/Extensions/LobbyViewComponents+Constants.swift
+++ PeekMovie/Modules/Lobby/LobbyPage/ViewComponents/Extensions/LobbyViewComponents+Constants.swift
@@ -12,6 +12,32 @@ extension LobbyViewComponents {
     
     enum Constants {
         
+        static let appLogoHeight: CGFloat = 90
+        
+        static let paddingXXS: CGFloat = 4
+        static let paddingXS: CGFloat = 6
+        static let paddingS: CGFloat = 10
+        static let padding: CGFloat = 16
+        static let paddingM: CGFloat = 24
+        static let paddingL: CGFloat = 36
+        static let paddingXL: CGFloat = 48
+        static let paddingXXL: CGFloat = 52
+        static let paddingXXXL: CGFloat = 96
+        static let paddingXXXXL: CGFloat = 144
+        
+        static let roomidTextFieldPlaceholder = "Room ID"
+        static let joinButtonTitleText = "JOIN"
+        static let createButtonTitleText = "Create Room"
+        static let logOutButtonTitleText = "Log Out"
+        
+        static let createRoomLabelText = "If you do not have room id"
+        
+        static let inputBlockShadowOffset: CGSize = .zero
+        static let inputBlockShadowColor: CGColor = UIColor.black.cgColor
+        static let inputBlockShadowRadius: CGFloat = 64
+        static let inputBlockShadowOpacity: Float = 1
+        
+        
     }
     
 }
--- PeekMovie/Modules/Lobby/LobbyPage/ViewComponents/LobbyViewComponents.swift
+++ PeekMovie/Modules/Lobby/LobbyPage/ViewComponents/LobbyViewComponents.swift
@@ -12,17 +12,17 @@ final class LobbyViewComponents {
     
     lazy var backgroundLinear = PeekBackgroundGradientGenerator.shared.getGradient(type: .linearBottomLeft)
     
-//    lazy var appLogoIconView = PeekIconView(image: Images.Logo.App.x90, size: .x90, cornerRadius: Constants.paddingM / 2, withShadow: true)
+    lazy var appLogoIconView = PeekIconView(image: Images.Logo.App.x90, size: .x90, cornerRadius: Constants.paddingM / 2, withShadow: true)
     
-//    lazy var roomIdTextField = PeekInputTextField(placeholder: Constants.usernameTextFieldPlaceholder)
+    lazy var roomIdTextField = PeekInputTextField(placeholder: Constants.roomidTextFieldPlaceholder)
     lazy var roomIdIconView = PeekIconView(image: Images.Icon.MovieRoom.x24)
     
-//    lazy var mainButton = PeekButton(type: .main, titleText: Constants.loginButtonTitleText)
-//    lazy var secondaryButton = PeekButton(type: .secondary, titleText: Constants.signUpButtonTitleText)
-//    lazy var tertiaryButton = PeekButton(type: .tertiary, titleText: Constants.forgotPasswordButtonTitleText)
+    lazy var mainButton = PeekButton(type: .main, titleText: Constants.joinButtonTitleText)
+    lazy var secondaryButton = PeekButton(type: .secondary, titleText: Constants.createButtonTitleText)
+    lazy var tertiaryButton = PeekButton(type: .tertiary, titleText: Constants.logOutButtonTitleText)
     
-//    lazy var createRoomLabelLabel = PeekLabel(type: .secondary, text: Constants.signUpLabelText, font: .caption1)
-//    lazy var createRoomHStack = getBottomHStackView(with: [alternatorLabelLabel, secondaryButton])
+    lazy var createRoomLabelLabel = PeekLabel(type: .secondary, text: Constants.createRoomLabelText, font: .caption1)
+    lazy var createRoomHStack = getBottomHStackView(with: [createRoomLabelLabel, secondaryButton])
     
     lazy var backgroundImage = PeekBackgroundImageView(image: Images.Static.EntranceBackground.normal)
     
@@ -30,19 +30,19 @@ final class LobbyViewComponents {
     
     lazy var tapHandlerView = PeekTapHandlerView()
     
-//    lazy var inputBlock: UIView = {
-//        let view = UIView()
-//        view.layer.cornerRadius = Constants.paddingM
-//        view.backgroundColor = Colors.backgroundPrimary
-//        view.translatesAutoresizingMaskIntoConstraints = false
-//
-//        view.layer.shadowOffset = Constants.inputBlockShadowOffset
-//        view.layer.shadowColor = Constants.inputBlockShadowColor
-//        view.layer.shadowRadius = Constants.inputBlockShadowRadius
-//        view.layer.shadowOpacity = Constants.inputBlockShadowOpacity
-//
-//        return view
-//    }()
+    lazy var inputBlock: UIView = {
+        let view = UIView()
+        view.layer.cornerRadius = Constants.paddingM
+        view.backgroundColor = Colors.backgroundPrimary
+        view.translatesAutoresizingMaskIntoConstraints = false
+
+        view.layer.shadowOffset = Constants.inputBlockShadowOffset
+        view.layer.shadowColor = Constants.inputBlockShadowColor
+        view.layer.shadowRadius = Constants.inputBlockShadowRadius
+        view.layer.shadowOpacity = Constants.inputBlockShadowOpacity
+
+        return view
+    }()
     
     
     func setupView(parent: UIView) {
@@ -58,5 +58,24 @@ final class LobbyViewComponents {
     }
  
     
+    
+}
+
+
+extension LobbyViewComponents {
+    
+    func getBottomHStackView(with arrangedSubviews: [UIView]) -> UIStackView {
+        let view = UIStackView()
+        view.axis = .horizontal
+        view.spacing = Constants.paddingXXS
+        view.backgroundColor = Colors.clearBlack
+        
+        for subview in arrangedSubviews {
+            view.addArrangedSubview(subview)
+        }
+        
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }
     
 }
